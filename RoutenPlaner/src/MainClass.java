6ae2031dcfb58f58db905ce0e38dd7e9d8f0741f
Robin Schneider <rschneidermails@gmail.com>
2018-06-15 21:00:57 +0200

    --all

--- RoutenPlaner/src/MainClass.java
+++ RoutenPlaner/src/MainClass.java
@@ -1,6 +1,17 @@
+import java.awt.*;
+import javax.swing.*;
 
 public class MainClass {
-	public static void main (String[] Args) {
-		System.out.println("Hello Hierarchen!");
+	public static void main(String[] args) {
+		JFrame mainFrame = new JFrame();
+		mainFrame.setTitle("Hierarchische RoutenPlanung");
+		mainFrame.setIconImage(new ImageIcon("C:\\\\Users\\\\User\\\\Downloads\\\\Taxi.jpg").getImage());
+		DrawCanvas canvas = new DrawCanvas();
+		canvas.setPreferredSize(new Dimension(400,400));
+		Container cp = mainFrame.getContentPane();
+		cp.add(canvas);
+		mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		mainFrame.pack();
+		mainFrame.setVisible(true); 
 	}
 }
