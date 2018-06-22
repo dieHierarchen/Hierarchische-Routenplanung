f8229dfd8463044ec7cd74b2767fab02ed7c53c7
Robin Schneider <rschneidermails@gmail.com>
2018-06-22 20:07:10 +0200

    --all

--- RoutenPlaner/src/MainClass.java
+++ RoutenPlaner/src/MainClass.java
@@ -3,20 +3,24 @@ import java.util.ArrayList;
 
 import javax.swing.*;
 
+import GUI.Presenter;
 import Logic.DijkstraAlgorithm;
 
 public class MainClass {
-	public static void main(String[] args) {
-		JFrame mainFrame = new JFrame();
-		mainFrame.setTitle("Hierarchische RoutenPlanung");
-		mainFrame.setIconImage(new ImageIcon("C:\\\\Users\\\\User\\\\Downloads\\\\Taxi.jpg").getImage());
-		DrawCanvas canvas = new DrawCanvas();
-		canvas.setPreferredSize(new Dimension(400,400));
-		Container cp = mainFrame.getContentPane();
-		cp.add(canvas);
-		mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		mainFrame.pack();
-		mainFrame.setVisible(true); 
+	public static void main(String[] args) throws InterruptedException {
+//		JFrame mainFrame = new JFrame();
+//		mainFrame.setTitle("Hierarchische RoutenPlanung");
+//		mainFrame.setIconImage(new ImageIcon("C:\\\\Users\\\\User\\\\Downloads\\\\Taxi.jpg").getImage());
+//		DrawCanvas canvas = new DrawCanvas();
+//		canvas.setPreferredSize(new Dimension(400,400));
+//		Container cp = mainFrame.getContentPane();
+//		cp.add(canvas);
+//		mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+//		mainFrame.pack();
+//		mainFrame.setVisible(true); 
+		
+		Presenter presenter = new Presenter();
+		presenter.startApplication();
 		
 		
 		//setting up a testing matrix (incidence list of a graph)
